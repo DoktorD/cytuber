15e06a7e8db3cc04e07600f86ab1ae4b313c173a
DoktorD <dogan.matija@mail.inet.hr>
2023-03-06 14:12:21 +0100

    Update cytube.js

--- cytube.js
+++ cytube.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         Cytube
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
@@ -10,23 +10,27 @@
 // ==/UserScript==
 
 (function() {
+    function eventTamper(el){
+        var dtMeta = $._data( el, "events" )
+        if(dtMeta.click){
+            var $el = $(el)
+            var handle = dtMeta.click.handler
+            $el.off("click")
+            $el.on("click",function(){ if (confirm("Are u sure about that?")) handle() })
+            console.log("event tamper", el, dtMeta)
+        }
+    }
     function mod(mutationsList){
         for (var mutation of mutationsList) {
             for(var added of mutation.addedNodes){
-                for (var el of added.find("button")){
-                    var dtMeta = $._data( el, "events" )
-                    if(dtMeta.click){
-                        console.log("event tamper", el, dtMeta)
-                        var handle = dtMeta.click.handler
-                        el.off("click")
-                        el.on("click",function(){ if (confirm("Are u sure about that?")) handle() })
-                    }
+                for (var el of $(added).find("button")){
+                    setTimeout(eventTamper,15, el)
                 }
             }
         }
     }
     $('<style>').load("https://raw.githubusercontent.com/DoktorD/cytuber/master/cytube.css").appendTo("head")
-    
+
 
     var targetNode = document.getElementById('queue');
     var config = {
@@ -35,17 +39,12 @@
         subtree: false
     };
 
-    for (var el of targetNode.find("button")){
-        var dtMeta = $._data( el, "events" )
-        if(dtMeta.click){
-            var handle = dtMeta.click.handler
-            el.off("click")
-            el.on("click",function(){ if (confirm("Are u sure about that?")) handle() })
-        }
+    for (var el of $(targetNode).find("button")){
+        eventTamper(el)
     }
     var observer = new MutationObserver(mod);
     observer.observe(targetNode, config);
-    
+
     console.log('Maurice ready!');
 
 })();
