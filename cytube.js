f50723b6a6bd74ddc028b32f28bc8275bb6d1cdc
DoktorD <dogan.matija@mail.inet.hr>
2023-03-06 12:19:41 +0100

    Update cytube.js

--- cytube.js
+++ cytube.js
@@ -1,6 +1,13 @@
 var LOADED = (typeof LOADED==="undefined") ? false : true;
 LOADED ? location.reload() : '';
 
+$.ajax({
+    url: "https://raw.githubusercontent.com/DoktorD/cytuber/master/cytube.css",
+    success: function(data) {
+        $("<style>").appendTo("head").html(data);
+    }
+})
+
 $('document').ready(()=>{
     console.log('Custom cytube.js ready!');
 })
