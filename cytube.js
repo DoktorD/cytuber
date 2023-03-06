faeb5fff60c66017b91d07d24f9fa3756cff9fda
DoktorD <dogan.matija@mail.inet.hr>
2023-03-06 12:43:33 +0100

    Update cytube.js

--- cytube.js
+++ cytube.js
@@ -1,7 +1,20 @@
-var LOADED = (typeof LOADED==="undefined") ? false : true;
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://cytu.be/r/offline69
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=cytu.be
+// @grant        none
+// ==/UserScript==
+
+(function() {
 
 $('<style>').load("https://raw.githubusercontent.com/DoktorD/cytuber/master/cytube.css").appendTo("head")
 
 $('document').ready(()=>{
     console.log('Custom cytube.js ready!');
 })
+
+})();
