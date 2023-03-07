70346df83a6635a92ea07741c5def08cf4c96ac1
DoktorD <dogan.matija@mail.inet.hr>
2023-03-07 15:47:00 +0100

    Update cytube.js

--- cytube.js
+++ cytube.js
@@ -1,50 +1,63 @@
 // ==UserScript==
-// @name         Cytube
+// @name         MauriceCytube
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.1
 // @description  try to take over the world!
-// @author       You
+// @author       Maurice
 // @match        https://cytu.be/r/offline69
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=cytu.be
 // @grant        none
 // ==/UserScript==
 
 (function() {
-    function eventTamper(el){
-        var dtMeta = $._data( el, "events" )
-        if(dtMeta.click){
-            var $el = $(el)
-            var handle = dtMeta.click.handler
-            $el.off("click")
-            $el.on("click",function(){ if (confirm("Are u sure about that?")) handle() })
-            console.log("event tamper", el, dtMeta)
-        }
-    }
-    function mod(mutationsList){
-        for (var mutation of mutationsList) {
-            for(var added of mutation.addedNodes){
-                for (var el of $(added).find("button")){
-                    setTimeout(eventTamper,15, el)
+    $('<style>').load("https://raw.githubusercontent.com/DoktorD/cytuber/master/cytube.css").appendTo("head")
+
+    window.onload = function(){
+        function eventTamper(el){
+            var dtMeta = $._data( el, "events" )
+            if(dtMeta.click[0]){
+                var $el = $(el)
+                if($el.hasClass("qbtn-play")||$el.hasClass("qbtn-delete")){
+                    var handle = dtMeta.click[0].handler
+                    console.log("event tamper", el, dtMeta.click, handle)
+                    $el.addClass("btn-danger")
+                    $el.off("click")
+                    $el.on("click",function(){
+                        if (confirm("Are u sure about that?")) {
+                            handle()
+                        }
+                    })
                 }
             }
         }
+        function mod(mutationsList){
+            console.log("dynamic tamper ", mutationsList)
+            for (var mutation of mutationsList) {
+                for(var added of mutation.addedNodes){
+                    setTimeout(function(){
+                        for (var el of $(added).find("button")){
+                            setTimeout(eventTamper,150, el)
+                        }
+                    },150)
+                }
+            }
+        }
+        var targetNode = document.getElementById('queue');
+        var config = {
+            attributes: false,
+            childList: true,
+            subtree: false
+        };
+        console.log("init tamper")
+        setTimeout( function() {
+            for (var el of $(targetNode).find("button")){
+                eventTamper(el)
+            }
+        },2000)
+        var observer = new MutationObserver(mod);
+        observer.observe(targetNode, config);
+        console.log('Maurice ready!');
     }
-    $('<style>').load("https://raw.githubusercontent.com/DoktorD/cytuber/master/cytube.css").appendTo("head")
 
-
-    var targetNode = document.getElementById('queue');
-    var config = {
-        attributes: false,
-        childList: true,
-        subtree: false
-    };
-
-    for (var el of $(targetNode).find("button")){
-        eventTamper(el)
-    }
-    var observer = new MutationObserver(mod);
-    observer.observe(targetNode, config);
-
-    console.log('Maurice ready!');
 
 })();
