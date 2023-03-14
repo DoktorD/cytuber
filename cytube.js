1b165e2ccdeae9ad365f22c29d94dfd4f2a04a44
DoktorD <dogan.matija@mail.inet.hr>
2023-03-14 11:58:45 +0100

    Update cytube.js

--- cytube.js
+++ cytube.js
@@ -1,108 +1,36 @@
-// Define emotes as <img> tags
-const quinPsycho = document.createElement("img");
-quinPsycho.src = "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_8ddaac2eaff94f219a6719a45b214126/default/dark/3.0.webp";
-quinPsycho.id = "quinPsycho";
 
-const batDisco = document.createElement("img");
-batDisco.src = "https://cdn.7tv.app/emote/61143ae59bf574f1fded6724/4x.webp";
-batDisco.id = "batDisco";
+// Define emotes
+const emotes = {
+	quinPsycho : "https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_8ddaac2eaff94f219a6719a45b214126/default/dark/3.0",
+	batDisco : "https://cdn.7tv.app/emote/61143ae59bf574f1fded6724/4x.webp",
+	AlienGathering : "https://cdn.7tv.app/emote/60ae6b4486fc40d488d0b324/4x.webp",
+	alizeePls : "https://cdn.7tv.app/emote/60af035e12d77014914b6d40/4x.webp",
+	gachiHOP : "https://cdn.7tv.app/emote/61577531b785e05aa26c1005/4x.webp",
+	GIGACHAT : "https://cdn.7tv.app/emote/616c2b9fd89696663cf352a2/4x.webp",
+	goosePls : "https://cdn.7tv.app/emote/60b1dbb1fdd2d7d7bdf6e0c3/4x.webp",
+	BBoomer : "https://cdn.betterttv.net/emote/5fcbde889d35484633452abe/3x.webp",
+	quinJAM : "https://cdn.betterttv.net/emote/5eb81632813f9216935560a3/3x.webp",
+	quinPls : "https://cdn.betterttv.net/emote/5e7281e96d485d372b29586a/3x.webp",
+	docPls : "https://cdn.betterttv.net/emote/5e2eaabeb1d0ac51e8ece290/3x.webp",
+	newMeta : "https://cdn.betterttv.net/emote/57192ed41b0211ae580c7a2a/3x.webp",
+	pepeD : "https://cdn.betterttv.net/emote/618a6b801f8ff7628e6d1a4a/3x.webp",
+	quinRoll : "https://cdn.betterttv.net/emote/5fb29cd042cf82644d87cc4c/3x.webp",
+	RainbowPls : "https://cdn.betterttv.net/emote/5b35cae2f3a33e2b6f0058ef/3x.webp",
+	POGGERS : "https://cdn.betterttv.net/emote/602bbdcfd049042e32dc47f1/3x.webp",
+	TriDance : "https://cdn.betterttv.net/emote/5d1e70f498539c4801cc3811/3x.webp",
+	ratJam : "https://cdn.betterttv.net/emote/5fc5ab31e22688461fed5ba6/3x.webp",
+	docJAM : "https://cdn.7tv.app/emote/604217e977137b000de9e680/4x.webp",
+}
 
-const AlienGathering = document.createElement("img");
-AlienGathering.src = "https://cdn.7tv.app/emote/60ae6b4486fc40d488d0b324/4x.webp";
-AlienGathering.id = "AlienGathering";
-
-const alizeePls = document.createElement("img");
-alizeePls.src = "https://cdn.7tv.app/emote/60af035e12d77014914b6d40/4x.webp";
-alizeePls.id = "alizeePls";
-
-const gachiHOP = document.createElement("img");
-gachiHOP.src = "https://cdn.7tv.app/emote/61577531b785e05aa26c1005/4x.webp";
-gachiHOP.id = "gachiHOP";
-
-const GIGACHAT = document.createElement("img");
-GIGACHAT.src = "https://cdn.7tv.app/emote/616c2b9fd89696663cf352a2/4x.webp";
-GIGACHAT.id = "GIGACHAT";
-
-const goosePls = document.createElement("img");
-goosePls.src = "https://cdn.7tv.app/emote/60b1dbb1fdd2d7d7bdf6e0c3/4x.webp";
-goosePls.id = "goosePls";
-
-const BBoomer = document.createElement("img");
-BBoomer.src = "https://cdn.betterttv.net/emote/5fcbde889d35484633452abe/3x.webp";
-BBoomer.id = "BBoomer";
-
-const quinJAM = document.createElement("img");
-quinJAM.src = "https://cdn.betterttv.net/emote/5eb81632813f9216935560a3/3x.webp";
-quinJAM.id = "quinJAM";
-
-const quinPls = document.createElement("img");
-quinPls.src = "https://cdn.betterttv.net/emote/5e7281e96d485d372b29586a/3x.webp";
-quinPls.id = "quinPls";
-
-const docPls = document.createElement("img");
-docPls.src = "https://cdn.betterttv.net/emote/5e2eaabeb1d0ac51e8ece290/3x.webp";
-docPls.id = "docPls";
-
-const newMeta = document.createElement("img");
-newMeta.src = "https://cdn.betterttv.net/emote/57192ed41b0211ae580c7a2a/3x.webp";
-newMeta.id = "newMeta";
-
-const pepeD = document.createElement("img");
-pepeD.src = "https://cdn.betterttv.net/emote/618a6b801f8ff7628e6d1a4a/3x.webp";
-pepeD.id = "pepeD";
-
-const quinRoll = document.createElement("img");
-quinRoll.src = "https://cdn.betterttv.net/emote/5fb29cd042cf82644d87cc4c/3x.webp";
-quinRoll.id = "quinRoll";
-
-const RainbowPls = document.createElement("img");
-RainbowPls.src = "https://cdn.betterttv.net/emote/5b35cae2f3a33e2b6f0058ef/3x.webp";
-RainbowPls.id = "RainbowPls";
-
-const POGGERS = document.createElement("img");
-POGGERS.src = "https://cdn.betterttv.net/emote/602bbdcfd049042e32dc47f1/3x.webp";
-POGGERS.id = "POGGERS";
-
-const TriDance = document.createElement("img");
-TriDance.src = "https://cdn.betterttv.net/emote/5d1e70f498539c4801cc3811/3x.webp";
-TriDance.id = "TriDance";
-
-const ratJam = document.createElement("img");
-ratJam.src = "https://cdn.betterttv.net/emote/5fc5ab31e22688461fed5ba6/3x.webp";
-ratJam.id = "ratJam";
-
-const docJAM = document.createElement("img");
-docJAM.src = "https://cdn.7tv.app/emote/604217e977137b000de9e680/4x.webp";
-docJAM.id = "docJAM";
-
-// Collect emotes
-const emotes = [
-    quinPsycho,
-    batDisco,
-    AlienGathering,
-    alizeePls,
-    gachiHOP,
-    GIGACHAT,
-    goosePls,
-    BBoomer,
-    quinJAM,
-    quinPls,
-    docPls,
-    newMeta,
-    pepeD,
-    RainbowPls,
-    POGGERS,
-    TriDance,
-    ratJam,
-    docJAM,
-];
-
-(function() {
+$(document).ready( ()=>{
     'use strict';
 
     const root = document.getElementsByTagName("body")[0];
 
-    for (let emote of emotes) {
-        root.appendChild(emote);
+    for (let emoteN in emotes) {
+		let img = document.createElement("img");
+		img.id = emoteN
+		img.src = emoteN
+        root.appendChild(img);
     }
-})();
+});
