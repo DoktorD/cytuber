9fa7fe8b4315129b684a515a68853226cc60296c
Matija <matija@nivas.hr>
2023-07-05 16:58:22 +0200

    w2g autoupdate

--- w2g.js
+++ w2g.js
@@ -1,15 +1,16 @@
 // ==UserScript==
-// @name         Watch2Gether
-// @namespace    https://w2g.tv/
-// @version      0.12
-// @description  u better watch yo self
-// @author       MauriceMann
-// @updateURL    https://raw.githubusercontent.com/DoktorD/cytuber/master/w2g.js
-// @match        https://w2g.tv/en/room/?access_key=cfd5n1dr6mg1cgrttahi6r
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=w2g.tv
-// @require      https://code.jquery.com/jquery-3.7.0.slim.min.js
-// @require      https://raw.githubusercontent.com/DoktorD/cytuber/master/emoteList.js
-// @grant        GFY
+// @name			Watch2Gether
+// @namespace		https://w2g.tv/
+// @version			0.12
+// @description		u better watch yo self
+// @author			MauriceMann
+// @downloadURL		https://raw.githubusercontent.com/DoktorD/cytuber/master/w2g.js
+// @updateURL		https://raw.githubusercontent.com/DoktorD/cytuber/master/w2g.js
+// @match			https://w2g.tv/en/room/?access_key=cfd5n1dr6mg1cgrttahi6r
+// @icon			https://www.google.com/s2/favicons?sz=64&domain=w2g.tv
+// @require			https://code.jquery.com/jquery-3.7.0.slim.min.js
+// @require			https://raw.githubusercontent.com/DoktorD/cytuber/master/emoteList.js
+// @grant			GFY
 // ==/UserScript==
 
 (function() {
